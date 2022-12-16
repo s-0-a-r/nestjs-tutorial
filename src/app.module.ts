e6e05e2085ec03f807953933c61f6a3257ee5e83
sho.tanaka <20152463+s-0-a-r@users.noreply.github.com>
2022-12-16 15:05:17 +0900

    setup graphql (#1)
    * build: add packages of graphql
    
    * fix: add missing packages and settings
    
    * build: use NestJS v8
    
    * feat: add resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './modules/events/events.module';
+import { SchedulesModule } from './modules/schedules/schedules.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+    }),
+    EventsModule,
+    SchedulesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
